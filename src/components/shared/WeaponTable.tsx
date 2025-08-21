51fda3e0b510f2e5fed97f31a6a2d7708468ae5a
vjosset <lkjasd>
2025-08-21 07:42:39 -0400

    Make weapon name clickable for selection in edit operative modal

--- src/components/shared/WeaponTable.tsx
+++ src/components/shared/WeaponTable.tsx
@@ -102,7 +102,7 @@ export default function WeaponTable({
                       { ' ' }
                       {weaponIcons[wep.wepType] ?? weaponIcons['E']}
                       { ' ' }
-                      {wep.wepName}
+                      <span onClick={() => onToggleWeapon && onToggleWeapon(wep.wepId)}>{wep.wepName}</span>
                     </td>
                     <td></td><td></td><td></td>
                   </tr>
